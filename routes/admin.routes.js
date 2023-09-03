7d956a9b5865aa3832e6bf06b824084ef0590b17
uchakip <syedasamra812@gmail.com>
2023-09-03 22:13:27 +0500

    18th commit

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,14 +2,15 @@ import { Router } from "express";
 import jwt from 'jsonwebtoken';
 import { userModel } from "../models/users.js";
 import { productModel } from "../models/womanwear.js";
-import multer, { diskStorage } from "multer";
+import multer from "multer";
 import { productModel2 } from "../models/makeup.js";
 import { productModel3 } from "../models/skincare.js";
 
 
 
 const adminRouter = Router();
-
+const upload = multer()
+adminRouter.use(upload.array())
 
 const authMiddleware= async (req,res, next) =>{
     console.log("auth middleware ");
@@ -39,45 +40,23 @@ const authMiddleware= async (req,res, next) =>{
 
 adminRouter.use(authMiddleware);
 
-const storage = diskStorage({
-    destination: (req, file, cb)=>{
-        cb(null, process.env.UPLOAD_PATH)
-    },
-    filename: (req, file, cb) =>{ 
-        
-        const filename = file.originalname.toLowerCase().split(' ').join('.')
-        cb(null, filename);
-    }
-});
 
-let upload=  multer({
-    storage: storage,
-    fileFilter: (req, file, cb)=>{
-        if(file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg"){
-            cb(null, true)
-        }
-        else {
-            cb(null, false)
-            return cb(new Error("only .jpg,.jpeg and .png extensions allowed"));
-        }
-    }
-})
 
-adminRouter.post('/createproduct/:collection' ,upload.single('cover') , async (req,res)=>{
+adminRouter.post('/createproduct/:collection', async (req,res)=>{
     try {
         console.log(req.body);
         const collection = req.params.collection;
-        const {title, description , userId, price, originalprice, discount , stock } = req.body;
+        const {title, description , userId, price, cover,cover2,originalprice, discount , stock } = req.body;
 
-        let uploadurl =  req.protocol + "://" +req.get("host") +"/" + process.env.UPLOAD_PATH +"/" + req.file.filename;
+       // let uploadurl =  req.protocol + "://" +req.get("host") +"/" + process.env.UPLOAD_PATH +"/" + req.file.filename;
 
       
 
         let newProduct;
         if (collection === "womanwear") {
             newProduct = await productModel.create({
-                cover: uploadurl,
-                cover2: uploadurl,
+                cover ,
+                cover2,
                 title, 
                 description,
                 price,
@@ -88,7 +67,7 @@ adminRouter.post('/createproduct/:collection' ,upload.single('cover') , async (r
             });
         } else if (collection === "makeup") {
             newProduct = await productModel2.create({
-                cover: uploadurl,
+                cover,
                 title, 
                 description,
                 price,
@@ -99,7 +78,7 @@ adminRouter.post('/createproduct/:collection' ,upload.single('cover') , async (r
             });
         } else if (collection === "skincare") {
             newProduct = await productModel3.create({
-                cover: uploadurl,
+                cover,
                 title, 
                 description,
                 price,
@@ -108,6 +87,7 @@ adminRouter.post('/createproduct/:collection' ,upload.single('cover') , async (r
                 stock,
                 user: userId,
             });
+            console.log(req.body)
         }
 
         res.status(201).json({ newProduct });
